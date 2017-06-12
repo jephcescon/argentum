fefd831c8be1d8a6df0f5d3d3e7cac7aeb49f167
Jepherson <Jephcescon@hotmail.com>
2017-06-12 00:03:07 -0300

    Criada classe Candlestick e tornada imutável

---
+++ src/main/java/br/com/alura/argentum/modelo/Candlestick.java
@@ -0,0 +1,48 @@
+package br.com.alura.argentum.modelo;
+
+import java.time.LocalDateTime;
+
+public final class Candlestick {
+	private final double abertura;
+	private final double fechamento;
+	private final double maximo;
+	private final double minimo;
+	private final double volume;
+	private final LocalDateTime data;
+	
+	
+	public Candlestick(double abertura, double fechamento, double maximo, double minimo, double volume,
+			LocalDateTime data) {
+		this.abertura = abertura;
+		this.fechamento = fechamento;
+		this.maximo = maximo;
+		this.minimo = minimo;
+		this.volume = volume;
+		this.data = data;
+	}
+	
+	public double getAbertura() {
+		return abertura;
+	}
+	public double getFechamento() {
+		return fechamento;
+	}
+	public double getMaximo() {
+		return maximo;
+	}
+	public double getMinimo() {
+		return minimo;
+	}
+	public double getVolume() {
+		return volume;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public boolean isAlta(){
+		return this.fechamento > this.abertura;
+	}
+	public boolean isBaixa(){
+		return this.fechamento < this.abertura;
+	}
+}
