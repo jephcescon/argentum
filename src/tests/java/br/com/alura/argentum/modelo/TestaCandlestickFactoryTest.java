b432561b26e27256bcfa027d8297a7e46d574cec
Jepherson <Jephcescon@hotmail.com>
2017-06-12 16:37:31 -0300

    Criada classe para testar CandlestickFactory com um metodo para testar valores minimo, maximo, abertura, fechamento e volume.

---
+++ src/tests/java/br/com/alura/argentum/modelo/TestaCandlestickFactoryTest.java
@@ -0,0 +1,34 @@
+package br.com.alura.argentum.modelo;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestaCandlestickFactoryTest {
+
+	@Test
+	public void sequenciaDeNegociacoesSimples() {
+		LocalDateTime hoje = LocalDateTime.now();
+
+		Negociacao negociacao1 = new Negociacao(40.0, 100, hoje);
+		Negociacao negociacao2 = new Negociacao(35.0, 100, hoje);
+		Negociacao negociacao3 = new Negociacao(45.0, 100, hoje);
+		Negociacao negociacao4 = new Negociacao(20.0, 100, hoje);
+
+		List<Negociacao> negociacoes = Arrays.asList(negociacao1, negociacao2, negociacao3, negociacao4);
+
+		CandlestickFactory fabrica = new CandlestickFactory();
+
+		Candlestick candle = fabrica.geraClandleParaData(negociacoes, hoje);
+		
+		Assert.assertEquals(20.0, candle.getMinimo(), 0.00001);
+		Assert.assertEquals(45.0, candle.getMaximo(), 0.00001);
+		Assert.assertEquals(40.0, candle.getAbertura(), 0.00001);
+		Assert.assertEquals(20.0, candle.getFechamento(), 0.00001);
+		Assert.assertEquals(14000.0, candle.getVolume(), 0.00001);
+	}
+
+}
