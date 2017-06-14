6f369af34d66d979736bf9b804ccaf9a8794b893
Jepherson <Jephcescon@hotmail.com>
2017-06-14 00:08:40 -0300

    Criada classe LeitorXML e sua respectiva classe para teste, criada uma classe de teste para a classe LocalDateTimeConverter

--- src/main/java/br/com/alura/argentum/reader/LeitorXml.java
+++ src/main/java/br/com/alura/argentum/reader/LeitorXml.java
@@ -1,20 +1,21 @@
 package br.com.alura.argentum.reader;
 
-import java.time.LocalDateTime;
+import java.io.InputStream;
+import java.util.List;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import br.com.alura.argentum.modelo.Negociacao;
+import br.com.alura.argentum.xstream.LocalDateTimeConverter;
 
 public class LeitorXml {
-	public static void main(String[] args) {
+	public List<Negociacao> carrega(InputStream inputStream){
+		XStream stream = new XStream(new DomDriver());
+		stream.registerLocalConverter(Negociacao.class, "data", new LocalDateTimeConverter());
+		stream.alias("negociacao", Negociacao.class);
 		
-		Negociacao negociacao = new Negociacao(10.0, 2, LocalDateTime.now());
-		
-		XStream xstream = new XStream(new DomDriver());
-		String xml = xstream.toXML(negociacao);
-		
-		System.out.println(xml);
+		return (List<Negociacao>) stream.fromXML(inputStream);
 	}
 }
+ 
