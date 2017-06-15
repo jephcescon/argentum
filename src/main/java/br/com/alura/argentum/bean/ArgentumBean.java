288a825186ebfaaccb49a6800bafbf7457931679
Jepherson <Jephcescon@hotmail.com>
2017-06-15 15:11:03 -0300

    Adicionado grafico de linhas

--- src/main/java/br/com/alura/argentum/bean/ArgentumBean.java
+++ src/main/java/br/com/alura/argentum/bean/ArgentumBean.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
 import br.com.alura.argentum.modelo.Negociacao;
 import br.com.alura.argentum.ws.ClientWebservice;
 
@@ -14,11 +17,26 @@ import br.com.alura.argentum.ws.ClientWebservice;
 public class ArgentumBean implements Serializable {
 	
 	private List<Negociacao> negociacoes;
+	private LineChartModel modeloGrafico;
 	
 	public ArgentumBean(){
 		this.negociacoes = new ClientWebservice().getNegociacoes();
+		this.modeloGrafico = new LineChartModel();
+		this.modeloGrafico.setTitle("Meu primeiro Grafico");
+		
+		LineChartSeries linha = new LineChartSeries();
+		linha.set(1, 10.5);
+		linha.set(2, 13.0);
+		linha.set(3, 12.0);
+		
+		this.modeloGrafico.addSeries(linha);
 	}
+	
 	public List<Negociacao> getNegociacoes(){
 		return this.negociacoes; 
+	}
+	
+	public LineChartModel getModeloGrafico() {
+		return modeloGrafico;
 	}
 }
